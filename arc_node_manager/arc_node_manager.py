87324fb45e382ef666c5f5d6ba8c56e32e344356
HowardWhile <makubex49gmail.com>
2023-04-13 12:42:30 +0800

    test event function

--- arc_node_manager/arc_node_manager.py
+++ arc_node_manager/arc_node_manager.py
@@ -1,16 +1,15 @@
 # This Python file uses the following encoding: utf-8
-import sys
-from PySide2.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QListWidget
 
-import os
-import rclpy
+# system module
+import sys
 import threading
 
-from rclpy.node import Node
-from os.path import exists
-from pathlib import Path
+# ui module
+from PySide2.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QListWidget
 
-from ament_index_python import get_resource
+# ros module
+import rclpy
+from std_msgs.msg import String
 
 
 class MainForm(QWidget):
@@ -27,39 +26,63 @@ class MainForm(QWidget):
     def ini_ros_node(self):
         rclpy.init(args=sys.argv)
         self.node = rclpy.create_node('node_manager')
+
         # Spin in a separate thread
         self.spin_thread = threading.Thread(
             target=rclpy.spin, args=(self.node, ), daemon=True)
         self.spin_thread.start()
 
+        self.publisher = self.node.create_publisher(String, 'topic', 10)
+
+        self.subscription = self.node.create_subscription(
+            String, 'topic', self.listener_callback, 10)
+
     def init_ui(self):
         # Create widgets
-        btn1 = QPushButton('Button 1')
-        btn2 = QPushButton('Button 2')
-        btn3 = QPushButton('Button 3')
-        lst = QListWidget()
+        self.btn_test = QPushButton('Button Test')
+        self.btn2 = QPushButton('Button 2')
+        self.btn3 = QPushButton('Button 3')
+        self.lst = QListWidget()
 
-        # Create layouts
+        # Create layout
         hbox = QHBoxLayout()
-        hbox.addWidget(btn1)
-        hbox.addWidget(btn2)
-        hbox.addWidget(btn3)
+        hbox.addWidget(self.btn_test)
+        hbox.addWidget(self.btn2)
+        hbox.addWidget(self.btn3)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
-        vbox.addWidget(lst)
+        vbox.addWidget(self.lst)
 
         # Set main layout
         self.setLayout(vbox)
 
         # Set window properties
         self.setWindowTitle('Layout Example')
-        self.setGeometry(100, 100, 300, 150)
+        self.setGeometry(50, 50, 800, 600)
 
         # Connect signals to slots
-        btn1.clicked.connect(lambda: lst.addItem('Button 1 clicked'))
-        btn2.clicked.connect(lambda: lst.addItem('Button 2 clicked'))
-        btn3.clicked.connect(lambda: lst.addItem('Button 3 clicked'))
+        # btn1.clicked.connect(lambda: lst.addItem('Button 1 clicked'))
+        # btn2.clicked.connect(lambda: lst.addItem('Button 2 clicked'))
+        # btn3.clicked.connect(lambda: lst.addItem('Button 3 clicked'))
+        self.btn_test.clicked.connect(self.btn1_clicked)
+        self.btn2.clicked.connect(self.btn2_clicked)
+        self.btn3.clicked.connect(self.btn3_clicked)
+
+    def btn1_clicked(self):
+        msg = String()
+        msg.data = 'Button 1 clicked'
+        self.publisher.publish(msg)
+
+    def btn2_clicked(self):
+        self.lst.addItem('Button 2 clicked')
+
+    def btn3_clicked(self):
+        self.lst.addItem('Button 3 clicked')
+
+    def listener_callback(self, msg):
+        self.lst.addItem(f"I heard: {msg.data}")
+        self.lst.scrollToBottom()
 
 
 def main(args=None):
